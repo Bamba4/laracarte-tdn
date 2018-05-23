24c9238221b761167528cc9f8003784cd27b0aab
MACBOOK <diagnec341@gmail.com>
2018-05-23 23:30:43 +0000

    Add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -10,5 +10,12 @@ if(! function_exists('pages_title')){
         else
             return $title . '|' . $base_title;
     }
+}
 
+
+
+if(! function_exists('set_active_route')){
+    function set_active_route($route) {
+        return Route::is($route) ? 'active' : ' ';
+    }
 }
